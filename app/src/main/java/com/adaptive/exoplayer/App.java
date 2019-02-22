70dd9f15b1d06a6edce80394999d55b4e72174c8
Mayur Solanki <mayursolanki120@gmail.com>
2019-02-22 16:40:11 +0530

    Adaptive Streaming with Exoplayer

---
+++ app/src/main/java/com/adaptive/exoplayer/App.java
@@ -0,0 +1,87 @@
+package com.adaptive.exoplayer;
+
+import android.app.Application;
+
+import com.google.android.exoplayer2.offline.DownloadManager;
+import com.google.android.exoplayer2.offline.DownloaderConstructorHelper;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.FileDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.upstream.cache.Cache;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
+import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor;
+import com.google.android.exoplayer2.upstream.cache.SimpleCache;
+import com.google.android.exoplayer2.util.Util;
+
+import java.io.File;
+
+public class App extends Application {
+
+    private static final String DOWNLOAD_ACTION_FILE = "actions";
+    private static final String DOWNLOAD_TRACKER_ACTION_FILE = "tracked_actions";
+    private static final String DOWNLOAD_CONTENT_DIRECTORY = "downloads";
+    private static final int MAX_SIMULTANEOUS_DOWNLOADS = 2;
+
+    protected String userAgent;
+
+    private File downloadDirectory;
+    private Cache downloadCache;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        userAgent = Util.getUserAgent(this, "ExoPlayerDemo");
+    }
+
+    /** Returns a {@link DataSource.Factory}. */
+    public DataSource.Factory buildDataSourceFactory() {
+        DefaultDataSourceFactory upstreamFactory =
+                new DefaultDataSourceFactory(this, buildHttpDataSourceFactory());
+        return buildReadOnlyCacheDataSource(upstreamFactory, getDownloadCache());
+    }
+
+    /** Returns a {@link HttpDataSource.Factory}. */
+    public HttpDataSource.Factory buildHttpDataSourceFactory() {
+        return new DefaultHttpDataSourceFactory(userAgent);
+    }
+
+    /** Returns whether extension renderers should be used. */
+    public boolean useExtensionRenderers() {
+        return "withExtensions".equals(BuildConfig.FLAVOR);
+    }
+
+
+
+    private synchronized Cache getDownloadCache() {
+        if (downloadCache == null) {
+            File downloadContentDirectory = new File(getDownloadDirectory(), DOWNLOAD_CONTENT_DIRECTORY);
+            downloadCache = new SimpleCache(downloadContentDirectory, new NoOpCacheEvictor());
+        }
+        return downloadCache;
+    }
+
+    private File getDownloadDirectory() {
+        if (downloadDirectory == null) {
+            downloadDirectory = getExternalFilesDir(null);
+            if (downloadDirectory == null) {
+                downloadDirectory = getFilesDir();
+            }
+        }
+        return downloadDirectory;
+    }
+
+    private static CacheDataSourceFactory buildReadOnlyCacheDataSource(
+            DefaultDataSourceFactory upstreamFactory, Cache cache) {
+        return new CacheDataSourceFactory(
+                cache,
+                upstreamFactory,
+                new FileDataSourceFactory(),
+                /* cacheWriteDataSinkFactory= */ null,
+                CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR,
+                /* eventListener= */ null);
+    }
+}
