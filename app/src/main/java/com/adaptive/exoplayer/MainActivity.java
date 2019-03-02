46a10acdcfac2c62f70aab6ae8c186cfa99f412a
Mayur Solanki <mayursolanki120@gmail.com>
2019-03-02 23:44:53 +0530

    Update MainActivity.java

--- app/src/main/java/com/adaptive/exoplayer/MainActivity.java
+++ app/src/main/java/com/adaptive/exoplayer/MainActivity.java
@@ -220,7 +220,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setProgress();
 
 
-        Log.e("Mac address ",""+getMacAddr());
+       
 
 
     }
@@ -734,30 +734,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public static String getMacAddr() {
-        try {
-            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface nif : all) {
-                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
-
-                byte[] macBytes = nif.getHardwareAddress();
-                if (macBytes == null) {
-                    return "";
-                }
-
-                StringBuilder res1 = new StringBuilder();
-                for (byte b : macBytes) {
-                    //res1.append(Integer.toHexString(b & 0xFF) + ":");
-                    res1.append(String.format("%02X:", b));
-                }
-
-                if (res1.length() > 0) {
-                    res1.deleteCharAt(res1.length() - 1);
-                }
-                return res1.toString();
-            }
-        } catch (Exception ex) {
-        }
-        return "02:00:00:00:00:00";
-    }
+    
 }
